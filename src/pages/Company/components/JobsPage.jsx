5e0c8c76c3e2185b99503936aec565aea6721a36
Sachin Kumar <stdtech7878@gmail.com>
2025-01-09 21:39:21 +0530

    Create Job Modal has been created

--- src/pages/Company/components/JobsPage.jsx
+++ src/pages/Company/components/JobsPage.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import CreateJob from './CreateJob'
+import React from "react";
+import CreateJob from "./CreateJob";
+import { Button, useDisclosure } from "@nextui-org/react";
+import CreateJobModal from "./CreateJobModal";
 
 function JobsPage() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   return (
-    <div className='px-2 sm:px-4 pb-12'>
-      <CreateJob/>
+    <div className="px-2 sm:px-4 pb-12">
+      <div className="flex flex-col gap-4">
+        {Array(2)
+          .fill(0)
+          .map((_, i) => (
+            <CreateJob key={i} />
+          ))}
+      </div>
+      <Button
+        onPress={onOpen}
+        radius="full"
+        className="mt-4 bg-[var(--main-color)] text-black"
+      >
+        New Job
+      </Button>
+      <CreateJobModal isOpen={isOpen} onOpenChange={onOpenChange} />
     </div>
-  )
+  );
 }
 
-export default JobsPage
+export default JobsPage;
