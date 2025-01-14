00861b33281ec8f6dce3dc18d34748aad1afd01c
Sachin Kumar <stdtech7878@gmail.com>
2025-01-14 13:16:57 +0530

    Communities has been updated

--- src/pages/Communities/components/CommunitiesHome.jsx
+++ src/pages/Communities/components/CommunitiesHome.jsx
@@ -1,24 +1,55 @@
-import React from 'react'
-import SinglePost from '../../../components/SinglePost'
-import { FaUserFriends } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import React from "react";
+import SinglePost from "../../../components/SinglePost";
+import { FaUserFriends } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const CommunitiesHome = () => {
-    return (
-        <>
-            {
-                Array(7).fill(0).map((_, i) => (
-                    <div className='mt-2 px-2 sm:px-4'>
-                        <Link to={"/communities/543534"} className='flex items-center gap-4 hover:underline opacity-70 font-bold'>
-                            <FaUserFriends />
-                            {"React js fans"}
-                        </Link>
-                        <SinglePost key={i} />
-                    </div>
-                ))
-            }
-        </>
-    )
-}
+  return (
+    <>
+      <div className="py-4 flex items-center justify-center flex-wrap gap-2">
+        {Array(11)
+          .fill(0)
+          .map((_, i) => (
+            <Link
+            key={i}
+              to={"/communities/78778"}
+              className="p-2 flex items-center flex-col gap-2 border border-transparent hover:border-[#c4c4c4] hover:dark:border-[#222222] box-content rounded-2xl"
+            >
+              <div className="h-32 w-32 rounded-2xl bg-gray-300 overflow-hidden">
+                <img
+                  className=" h-full object-cover"
+                  src="https://pbs.twimg.com/community_banner_img/1579498877941649408/ib8y5Ccy?format=jpg&name=360x360"
+                  alt=""
+                />
+              </div>
+              <div className="flex flex-col text-center">
+                <span className="font-semibold hover:underline">React/ Next JS Devs</span>
+                <span className="">
+                  <span className="font-semibold">{"2.6K"}</span>
+                  <span className="opacity-70"> Members</span>
+                </span>
+              </div>
+            </Link>
+          ))}
+      </div>
+      <div className="border-t-[0.5px] border-color">
+        {Array(7)
+          .fill(0)
+          .map((_, i) => (
+            <div className="mt-2 px-2 sm:px-4">
+              <Link
+                to={"/communities/543534"}
+                className="flex items-center gap-4 hover:underline opacity-70 font-bold"
+              >
+                <FaUserFriends />
+                {"React js fans"}
+              </Link>
+              <SinglePost key={i} />
+            </div>
+          ))}
+      </div>
+    </>
+  );
+};
 
-export default CommunitiesHome
+export default CommunitiesHome;
--- src/pages/Communities/components/SingleCommunity/SingleCommunity.jsx
+++ src/pages/Communities/components/SingleCommunity/SingleCommunity.jsx
@@ -45,59 +45,57 @@ const SingleCommunity = () => {
 
   return (
     <>
-      <div className="">
-        <img
-          className="w-full"
-          src="https://pbs.twimg.com/community_banner_img/1579498877941649408/ib8y5Ccy?format=jpg&name=360x360"
-          alt=""
-        />
-        <div className="relative px-2 sm:px-4 flex flex-col">
-          <div className="mt-2 flex items-start justify-between">
-            <span className=" text-3xl font-semibold">NExt DEvs</span>
-            <Dropdown placement="bottom-end">
-              <DropdownTrigger>
-                <div className="cursor-pointer">
-                  <BsThreeDots size={24} />
-                </div>
-              </DropdownTrigger>
-              <DropdownMenu aria-label="Profile Actions" variant="flat">
-                <DropdownItem key="settings">
-                  <Link
-                    onClick={onOpen}
-                    className="block w-full"
-                    to={"#"}
-                  >
-                    Edit Community Settings
-                  </Link>
-                </DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-            <EditCommunityModal isOpen={isOpen} onOpenChange={onOpenChange} />
-          </div>
-          <span className="mt-2 text-xl">
+      <div className="px-2 sm:px-4 flex flex-col">
+        <div className="self-end">
+          <Dropdown placement="bottom-end">
+            <DropdownTrigger>
+              <div className="cursor-pointer">
+                <BsThreeDots size={24} />
+              </div>
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Profile Actions" variant="flat">
+              <DropdownItem key="settings">
+                <Link onClick={onOpen} className="block w-full" to={"#"}>
+                  Edit Community Settings
+                </Link>
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+          <EditCommunityModal isOpen={isOpen} onOpenChange={onOpenChange} />
+        </div>
+        <div className="max-w-[200px] max-h-[200px] overflow-hidden rounded-2xl self-center">
+          <img
+            className="max-w-full max-h-full"
+            src="https://cdn.pixabay.com/photo/2016/12/28/08/15/hummingbird-1935665_1280.png"
+            alt=""
+          />
+        </div>
+        <div className="relative px-2 sm:px-4 flex flex-col items-center">
+          <span className="mt-2 text-3xl font-semibold text-center">
+            NExt DEvs
+          </span>
+          <span className="mt-2 text-xl text-center">
             👽Welome to the {"NExt DEvs"} Community
           </span>
-          <div className="mt-2 flex items-center justify-between gap-4">
-            <Link
-              to={"/communities/4545/members"}
-              className="px-2 flex items-center gap-4 hover:bg-gray-200 dark:hover:bg-gray-900"
-            >
-              <AvatarGroup>
-                {Array(3)
-                  .fill(0)
-                  .map((_, i) => (
-                    <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
-                  ))}
-              </AvatarGroup>
-              <span>
-                <span className="font-semibold">860</span> Members
-              </span>
-            </Link>
-            <div className="mr-2 sm:mr-4">
-              <Button radius="full" variant="bordered">
-                Join
-              </Button>
-            </div>
+          <Link
+            to={"/communities/4545/members"}
+            className="mt-2 px-2 flex items-center gap-4 hover:bg-gray-200 dark:hover:bg-gray-900"
+          >
+            <AvatarGroup>
+              {Array(3)
+                .fill(0)
+                .map((_, i) => (
+                  <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+                ))}
+            </AvatarGroup>
+            <span>
+              <span className="font-semibold">860</span> Members
+            </span>
+          </Link>
+          <div className="mt-2 mr-2 sm:mr-4">
+            <Button radius="full" className="bg-[var(--main-color)] text-black">
+              Join
+            </Button>
           </div>
         </div>
       </div>
